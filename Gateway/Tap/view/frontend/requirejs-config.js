57ca53e758b9389c72c96936e3db253d29c0a550
waqas1981 <w.zeshan@tap.company>
2020-06-26 22:03:44 +0500

    require js fix

--- Gateway/Tap/view/frontend/requirejs-config.js
+++ Gateway/Tap/view/frontend/requirejs-config.js
@@ -1,9 +1,5 @@
 var config = {
-    map: {
-        '*': {
-            hello:           'Gateway_Tap/js/hello',
-        }
-    },
+   
     paths: {
         'goSell': 'https://secure.gosell.io/js/sdk/tap.min'
     }
