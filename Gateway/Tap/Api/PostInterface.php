87879ade13914190d62dceead961d84c4e0645e8
Waqas Zeeshan <w.zeshan@itg.com.kw>
2022-02-28 14:58:25 +0500

    Adding apple pay feature in plugin

---
+++ Gateway/Tap/Api/PostInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace Gateway\Tap\Api;
+interface PostInterface
+{
+    /**
+     * GET for Post api
+     * @param string $value
+     * @return string
+     */
+ 
+    public function getPost();
+}
